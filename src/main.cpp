73349daa2db5b351022bbd2ccf47b8d523f10068
Paviel <prakapienka.work@gmail.com>
2026-08-25 22:30:59 +0200

    Expose minimal WASM synth API

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "synth/FmVoice.h"
+
+#include <memory>
+
+extern "C" {
+
+FmVoice* createSynth(double sampleRate) {
+    return new FmVoice(sampleRate);
+}
+
+void destroySynth(FmVoice* synth) {
+    delete synth;
+}
+
+void noteOn(FmVoice* synth, int midiNote, double velocity) {
+    if (synth != nullptr) {
+        synth->noteOn(midiNote, velocity);
+    }
+}
+
+void noteOff(FmVoice* synth) {
+    if (synth != nullptr) {
+        synth->noteOff();
+    }
+}
+
+double renderSample(FmVoice* synth) {
+    return synth != nullptr ? synth->renderSample() : 0.0;
+}
+
+}
+
+int main() {
+    return 0;
+}
